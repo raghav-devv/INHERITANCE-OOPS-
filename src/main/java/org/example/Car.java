e2cc35858a1205b8582fc40420628f7c38ab826e
Raghav <raghavkhajuria787@gmail.com>
2024-09-19 15:47:34 +0530

    Update Car.java

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,6 +1,8 @@
 package org.example;
+
 //Define a subclass for cars that extends the Vehicle class
 public class Car extends Vehicle{
+    
     // Fields to store the car's brand and speed
    String brand;
     int speed;
@@ -13,6 +15,7 @@ public class Car extends Vehicle{
         this.brand=brand;
         this.speed=speed;
     }
+    
     // Method to display the car's details
     public void valueof(){
         // Print the car's brand speed and type to the console
@@ -20,12 +23,14 @@ public class Car extends Vehicle{
         System.out.println("car type is:"+type+", model is: "+brand+", speed of:"+speed);
     // I call Vehicle type also in it , if i want i can directly call method that i created in vehicle class
     }
-//// Main class to test the Car class
+    
+// Main class to test the Car class
     public static void main(String[] args) {
         Car c1 = new Car("Electric","BMW",100);
 
         // Call the valueof method to display the car's details
         c1.valueof();
+        
         //here i did not call typeof method that was present in the vehicle class
 
     }
